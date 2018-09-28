354bd9bc1b77546945c137c293f292c95cb1d0bc
Juan I. Forcén <jiforcen@gmail.com>
2018-09-28 18:33:54 +0200

    Update lstmrecognizer.cpp

--- src/lstm/lstmrecognizer.cpp
+++ src/lstm/lstmrecognizer.cpp
@@ -187,10 +187,12 @@ void LSTMRecognizer::RecognizeLine(const ImageData& image_data, bool invert,
   }
   std::cout << "LSTMRecognizer::RecognizeLine" << std::endl;
   const UNICHARSET* unicharset = &GetUnicharset();  
+    /*
   for (int t = 0; t < unicharset->size(); t++) {
     std::cout << "unicharset->get_enabled(" << t <<"): ";
     std::cout << unicharset->get_enabled(t) << std::endl; 
   }       
+  */
   search_->Decode(outputs, kDictRatio, kCertOffset, worst_dict_cert,
                   &GetUnicharset(), glyph_confidences);    
   search_->ExtractBestPathAsWords(line_box, scale_factor, debug,
